cd419abf2a82be8898eb95d4a2b19ee3a3c686d7
DevWebAma <148153050+DevWebAma@users.noreply.github.com>
2024-09-06 00:54:30 +0200

    ma structure de base mvc

---
+++ app/config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+$title ='';
+$content ='';
+
+define('DB_HOST','localhost:8889');
+define('DB_NAME','book_hunter_2024');
+define('DB_USERS','root');
+define('DB_PASSWORD','root');
---
+++ app/views/templates/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <?php include_once'../app/views/partials/_head.php'; ?>
+</head>
+
+<body class="bg-gray-900 text-white font-sans leading-normal tracking-normal">
+  <!-- Header -->
+  <?php include'../app/views/partials/_header.php'; ?>
+
+  <!-- Main -->
+  <div class="container mx-auto flex flex-wrap pt-4 pb-12 text-white">
+  <!-- Aside -->
+  <?php include'../app/views/partials/_aside.php'; ?>
+  <!-- Main content -->
+  <?php include'../app/views/partials/_main.php'; ?>
+  </div>
+
+    <!-- Footer -->
+  <?php include'../app/views/partials/_footer.php';?>
+</body>
+</html>
